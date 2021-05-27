0712e399906205efe7a1364439e51fd2faf126ff
Vivian Tanaka <vivian.tanaka@mercadolivre.com>
2021-05-27 11:58:44 -0300

    Funcoes e estrutura inicial

---
+++ src/main/java/com/grupo3/calculadoram2/resources/CasaController.java
@@ -0,0 +1,12 @@
+package com.grupo3.calculadoram2.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CasaController {
+
+
+}
