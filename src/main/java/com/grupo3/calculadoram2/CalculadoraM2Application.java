f67420fa97852ba0ca7a0543f8ec7587ce4f13bc
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-05-27 11:37:27 -0300

    Project created

---
+++ src/main/java/com/grupo3/calculadoram2/CalculadoraM2Application.java
@@ -0,0 +1,13 @@
+package com.grupo3.calculadoram2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraM2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraM2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/calculadoram2/CalculadoraM2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.calculadoram2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraM2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
